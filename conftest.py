f06dc61e0d747b839afd2cdb6c390bd434caafed
sudharsan <sudhawell2023@gmail.com>
2024-07-20 22:27:13 +0530

    All Buy Crypto _ Frame Work

--- conftest.py
+++ conftest.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from pathlib import Path
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -26,3 +29,17 @@ def init_driver(request):
     yield
     time.sleep(3)
     driver.quit()
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    today = datetime.now()
+    report_dir = Path("ABC_report", today.strftime("%Y%m%d"))
+    report_dir.mkdir(parents=True, exist_ok=True)
+    pytest_html = report_dir / f"Report_{today.strftime('%Y%m%d%H%M')}.html"
+    config.option.htmlpath = pytest_html
+    config.option.self_contained_html = True
+
+
+def pytest_html_report_title(report):
+    report.title = "ABC Test Report"
